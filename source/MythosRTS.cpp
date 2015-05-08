302b849014f8b4d69f8fc10c5a81403e88b9db4a
Tanis Newlun <tanishehe@gmail.com>
2015-05-08 12:17:08 -0500

    GOT THAT CMAKE WORKING DO' {caps because excited}

--- source/MythosRTS.cpp
+++ source/MythosRTS.cpp
@@ -1,4 +1,12 @@
-int main(){
-	printf("Hello World!");
+#include "stdio.h"
+#include <stdlib.h>
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+int main(int argc, char* argv[]){
+	glutInit (& argc, argv);
+	glutCreateWindow ("GLEW Test");
+	GLenum err = glewInit ();
 	return 0;
 }
