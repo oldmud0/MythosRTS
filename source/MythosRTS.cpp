b94c4c8fec52e27146a01bf20555dba4ba1a42a7
Tanis Newlun <tanishehe@gmail.com>
2015-05-08 12:25:42 -0500

    Getting windows and such setup

--- source/MythosRTS.cpp
+++ source/MythosRTS.cpp
@@ -4,9 +4,25 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
+static int make_resources(void){
+    return 1;
+}
+
+static void update_fade_factor(void){
+}
+
+static void render(void){
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+
 int main(int argc, char* argv[]){
-	glutInit (& argc, argv);
-	glutCreateWindow ("GLEW Test");
-	GLenum err = glewInit ();
-	return 0;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(400, 300);
+	glutCreateWindow("Hello World");
+	glutDisplayFunc(&render);
+	glutIdleFunc(&update_fade_factor);
 }
